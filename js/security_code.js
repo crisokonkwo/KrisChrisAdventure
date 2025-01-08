824fac79ee73632ab5d21ef83600e84ce2a31d87
ChrisO <crisokonkwo@gmail.com>
2025-01-08 18:49:56 -0500

    Updated index and invitation

--- js/security_code.js
+++ js/security_code.js
@@ -1,11 +1,15 @@
 // Security Code Validation
 function validateCode() {
     const securityCodeInput = document.getElementById('security-code').value.trim();
-    const correctCode = 'WEDDING2025';
+    // const correctCode = 'WEDDING2025';
+    const encryptedCorrectCode = '5b737210ccf1653e001cb82ef8cb264c';
+    const encryptedInputCode = CryptoJS.MD5(securityCodeInput).toString();
+    console.log(encryptedInputCode);
+
     const rsvpButton = document.getElementById('rsvp-button');
     const errorMessage = document.getElementById('error-message');
 
-    if (securityCodeInput === correctCode) {
+    if (encryptedInputCode === encryptedCorrectCode) {
         // Hide security input and show RSVP button
         document.getElementById('security-container').style.display = 'none';
         rsvpButton.style.display = 'inline-block';
